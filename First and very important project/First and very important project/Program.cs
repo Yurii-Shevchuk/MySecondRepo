bfea464f11c2509be86389fa135aee7897a72e8a
Yurii-Shevchuk <yuriishvchk@gmail.com>
2023-08-31 12:56:26 +0300

    Initial commit

---
+++ First and very important project/First and very important project/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World, my name is Yurii! What's yours?");
+string strangerName = "";
+while(strangerName == null || strangerName == String.Empty)
+{
+    strangerName = Console.ReadLine();
+}
+
+Console.WriteLine($"Nice to meet you, {strangerName}, have a nice day!");
+Console.ReadLine();
